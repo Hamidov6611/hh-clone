72c237be4d382a7e288581436711edea40307683
Hamidov6611 <dilmuroddelitot@gmail.com>
2023-11-01 15:04:38 +0500

    Route page

--- app/index.js
+++ app/index.js
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text } from "react-native";
+import React, { useCallback } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native-gesture-handler";
+import { MyJobs, PopularJobs, Search } from "../components";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function index() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("../assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
-  )
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "#eee" }}
+      onLayout={onLayoutRootView}
+    >
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <Search />
+        <MyJobs />
+        <PopularJobs />
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
